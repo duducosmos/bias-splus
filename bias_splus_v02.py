c4d51941bb9d34556dcc5fd5432224b40e246211
waugusto <walter.augusto@gmail.com>
2016-11-11 14:14:37 -0200

    First non-tested 0.3 version

--- bias_splus_v02.py
+++ bias_splus_v02.py
@@ -45,6 +45,8 @@ from datetime import date, timedelta
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
 from matplotlib.dates import DayLocator, DateFormatter, date2num
+from pymongo import MongoClient
+import time
 
 """
 FUNCTIONS
@@ -83,74 +85,13 @@ def outputPlot(fulldate, xdates, ypoints, errors, outfile, perc):
 def daterange(start_date, end_date):
     for ordinal in range(start_date.toordinal(), end_date.toordinal()):
         yield date.fromordinal(ordinal)
-        
-"""
-def getFilelistsDates(init, final):
+
+def getDatesLists(init, final):
     list_dates = daterange(init, final)
-    filelist = ['filelist_'+str(d.year)+('%02d' % d.month)+('%02d' % d.day)+'.csv' for d in list_dates]
-    return filelist
-"""
+    return [d for d in list_dates], [str(d.year)+('%02d' % d.month)+('%02d' % d.day) for d  in list_dates]
 
-"""
-def downloadFilelistsCSV(filelist):
-    csvlist = list()
-    cmd = 'wget --no-check-certificate -c https://t80s_images:t80s_images_keywords_pass@splus.astro.ufsc.br/'
 
-    for f in filelist:
-        if os.path.isfile(f):
-            csvlist.append(f)
-        else:
-            cmd_wget = cmd+f
-            os.system(cmd_wget)
-            if os.path.isfile(f):
-                csvlist.append(f)
-    return csvlist
-"""
-"""
-def downloadNewFilelistsCSV(filelist):
-    csvlist = list()
-    cmd = 'wget --no-check-certificate -c https://t80s_images:t80s_images_keywords_pass@splus.astro.ufsc.br/'
-    
-    for f in filelist:
-        if os.path.isfile(f) == False:
-            cmd_wget = cmd+f
-            os.system(cmd_wget)
-            if os.path.isfile(f):
-                csvlist.append(f)
-                
-    return csvlist        
-"""     
 
-""" 
-def getBiaslistFromCSV(csvfile):
-    biaslist = list()
-    biasnames = list()
-    f = open(csvfile, 'r')
-    for line in f:
-        line = line.strip()
-        columns = line.split()
-        pathfile = columns[2]
-        namefile = columns[2].split('/')[-1]
-        for c in columns[3].split(','):
-            if c == 'ZERO':
-                biaslist.append(pathfile)
-                biasnames.append(namefile)
-    return biaslist, biasnames
-"""     
-"""     
-def convertCSVtoDate(csvfile):
-    d = csvfile[9:-4]
-    return date(int(d[0:4]), int(d[4:6]), int(d[6:8]))
-"""
-    
-def downloadBiasFromDate(biaslist):
-    cmd = 'wget --no-check-certificate -c '
-    for b in biaslist:
-        namefile = b.split('/')[-1]
-        if os.path.isfile(namefile) == False:
-            cmd_get = cmd+b
-            os.system(cmd_get)
-    
 """
 MAIN
 """
@@ -164,9 +105,14 @@ parser.add_argument('-o','--output', default=None, help='output file name, also 
                      will be given if None', metavar='output_file')
 parser.add_argument('-m','--master', default='splus_master_bias.fits', help='input \
                      master bias image', metavar='MASTER_BIAS.fits')
-parser.add_argument('-p','--perc', default=DEFAULT_ACCEPTABLE_PERC, type=float,
+parser.add_argument('--perc', default=DEFAULT_ACCEPTABLE_PERC, type=float,
                     help='percentage above which the bias is flagged \
                     as not acceptable')
+parser.add_argument('--host', help='The host of the mongo database. \
+                    Mandatory if not ', type=str, default='192.168.20.118')
+parser.add_argument('--port', help='The port of the mongo database. \
+                    Mandatory if not ', type=str, default='27017')
+parser.add_argument('--path', help='path to the images', type=str, default='27017')
 
 #command line example:
 """
@@ -186,25 +132,21 @@ Parse the input parameters, define and declare variables
 args = parser.parse_args()
 settings = vars(args)
 
+host = settings['host']
+port = settings['port']
+path = settings['path']
 
-if ((settings['initdate'] and settings['finaldate']) != None):
+client = MongoClient('%s:%s' % (host, port))
+
+if ((settings['initdate'] and settings['finaldate']) is not None):
     initDate = date(int(settings['initdate'].split('/')[0]),int(settings['initdate'].split('/')[1]),int(settings['initdate'].split('/')[2]))
     finalDate = date(int(settings['finaldate'].split('/')[0]),int(settings['finaldate'].split('/')[1]),int(settings['finaldate'].split('/')[2]))
-    list_dates = daterange(initDate, finalDate)
-    datelist = getFilelistsDates(initDate,finalDate)
-    csvlist = downloadFilelistsCSV(datelist)
-    fulldatelist = [d for d in list_dates]
-    actualdates = map(convertCSVtoDate, csvlist)
+    fulldatelist, datelist = getDatesLists(initDate,finalDate)
 else:
     initDate = date(int(DEFAULT_INITIAL_DATE.split('/')[0]),int(DEFAULT_INITIAL_DATE.split('/')[1]),int(DEFAULT_INITIAL_DATE.split('/')[2]))
     finalDate = date.today()
-    datelist = getFilelistsDates(initDate,finalDate)
-    csvlist = downloadNewFilelistsCSV(datelist)
-    actualdates = map(convertCSVtoDate, csvlist)
-    fulldatelist = actualdates
-
-acceptablePerc = settings['perc']
-
+    fulldatelist, datelist = getDatesLists(initDate,finalDate)
+    
 if(settings['output']==None):
     outputFilename = 'bias_splus'+str(finalDate)
 else:
@@ -221,64 +163,64 @@ except:
     print('ERROR: Could not find or access data of master bias named: '+masterFilename)
     fout.write('\n ERROR: Could not find or access data of master bias named: '+masterFilename+'\n')
     raise
-    
+
+acceptablePerc = settings['perc']
+
 bias_day_mean = list()
 bias_day_error = list()
+actualdates = datelist
 
-for csv in csvlist:
-    dateback = convertCSVtoDate(csv)
-    fout.write('\n\n# All bias for date '+ str(dateback)+' ...')
-    fout.write('\n# date filename mean median max min std mean_per_master median_per_master std_per_master')
-    print '\n\n# All bias for date '+ str(dateback)+' ...'
-    print '# date filename mean median max min std mean_per_master median_per_master std_per_master'
-    biasfilelist, biasnameslist = getBiaslistFromCSV(csv)
-    downloadBiasFromDate(biasfilelist)
-    
-    nbias = len(biasnameslist)
-   
-    bias_stack = np.zeros((nbias,m_nx,m_ny),dtype=np.float32)
-    bias_stack_per_master = np.zeros((nbias,m_nx,m_ny),dtype=np.float32)
-    
-    i = 0
-    for b in biasnameslist:
-        try:
-            _bdata = fits.open(b)[0].data
-            nx, ny = np.shape(_bdata)
-            if (nx != m_nx or ny != m_ny):
-		raise		
-            bdata = np.zeros((nx,ny),dtype=np.float32)
-            bdata += _bdata
-        except:
-            print('WARNING: Unable to open or corrupted file '+b+'. Ignoring it.\n')
-            fout.write('\n WARNING: Unable to open or corrupted file '+b+'. Ignoring it.\n')
-            bias_stack = np.delete(bias_stack,-1,axis=0)
-            nbias-=1
-        else:
-            data_per_master = bdata/mdata
-            print dateback, b, int(np.mean(bdata)), int(np.median(bdata)), int(np.max(bdata)), int(np.min(bdata)), np.std(bdata), np.mean(data_per_master), np.median(data_per_master), np.std(data_per_master)
-            fout.write('\n'+str(dateback)+' '+b+' '+str(int(np.mean(bdata)))+' '+str(int(np.median(bdata)))+' '+str(int(np.max(bdata)))+' '+str(int(np.min(bdata)))+' '+str(np.std(bdata))+' '+str(np.mean(data_per_master))+' '+str(np.median(data_per_master))+' '+str(np.std(data_per_master)))
-            bias_stack[i] = bdata
-            bias_stack_per_master[i] = bdata/mdata
-            i+=1
-   
-    if(nbias == 0):
-        print('WARNING: There are no suitable bias files for date '+str(dateback)+'!')
-        fout.write('\n WARNING: There are no suitable bias files for date '+str(dateback)+'!')
-        actualdates.remove(dateback)
+for night in datelist:
+    cursor = client.images.fits_keywords.find({'_night': night,'IMAGETYP': 'ZERO'}).sort([('OBJECT', 1), ('FILENAME', 1), ])
+    nbias = cursor.count()
+    if nbias <= 0:
+        print('WARNING: There are no suitable bias files for date '+night+'!')
+        fout.write('\n WARNING: There are no suitable bias files for date '+night+'!')
+        actualdates.remove(night)
     else:
-        fout.write('\n# Combined ' + str(nbias) + 'bias for the date '+ str(dateback)+' ...')
-        fout.write('\n# date mean median max min std mean_per_master median_per_master std_per_master')
-        print '# Combined ' + str(nbias) + ' bias for the date '+ str(dateback)+' ...'
-        print '# date mean median max min std mean_per_master median_per_master std_per_master'
-        stacked_bias = np.median(bias_stack,axis=0)
-        stacked_bias_per_master = stacked_bias/mdata
-        fout.write('\n'+str(dateback)+' '+str(int(np.mean(stacked_bias)))+' '+str(int(np.median(stacked_bias)))+' '+ str(int(np.max(stacked_bias)))+' '+ str(int(np.min(stacked_bias)))+' '+ str(np.std(stacked_bias))+' '+str(np.mean(stacked_bias_per_master))+' '+str(np.median(stacked_bias_per_master))+' '+str(np.std(stacked_bias_per_master)))
-        print dateback, int(np.mean(stacked_bias)), int(np.median(stacked_bias)), int(np.max(stacked_bias)), int(np.min(stacked_bias)), np.std(stacked_bias), np.mean(stacked_bias_per_master), np.median(stacked_bias_per_master), np.std(stacked_bias_per_master)
-        if((np.mean(stacked_bias_per_master) >= (1.0+acceptablePerc)) or (np.mean(stacked_bias_per_master) <= (1.0-acceptablePerc))):
-            print('WARNING: Bias for the date '+ str(dateback)+'above acceptable value compared to master!')
-            fout.write('\n WARNING: Bias for the date '+ str(dateback)+'above acceptable value compared to master! \n')
-        bias_day_mean.append(np.mean(stacked_bias_per_master))
-        bias_day_error.append(np.std(stacked_bias_per_master))
+        fout.write('\n\n# All bias for date '+ night+' ...')
+        fout.write('\n# date filename mean median max min std mean_per_master median_per_master std_per_master')
+        print '\n\n# All bias for date '+ night+' ...'
+        print '# date filename mean median max min std mean_per_master median_per_master std_per_master'
     
+        biasfilelist = [str(frame['FILENAME']) for frame in cursor]
+        bias_stack = np.zeros((nbias,m_nx,m_ny),dtype=np.float32)
+        bias_stack_per_master = np.zeros((nbias,m_nx,m_ny),dtype=np.float32)
+        i = 0
+        for b in biasfilelist:
+            try:
+                _bdata = fits.open(path+'/'+night+'/'+b)[0].data
+                nx, ny = np.shape(_bdata)
+                if (nx != m_nx or ny != m_ny):
+                    raise		
+                bdata = np.zeros((nx,ny),dtype=np.float32)
+                bdata += _bdata
+            except:
+                print('WARNING: Unable to open or corrupted file '+b+'. Ignoring it.\n')
+                fout.write('\n WARNING: Unable to open or corrupted file '+b+'. Ignoring it.\n')
+                bias_stack = np.delete(bias_stack,-1,axis=0)
+                nbias-=1
+            else:
+                data_per_master = bdata/mdata
+                print night, b, int(np.mean(bdata)), int(np.median(bdata)), int(np.max(bdata)), int(np.min(bdata)), np.std(bdata), np.mean(data_per_master), np.median(data_per_master), np.std(data_per_master)
+                fout.write('\n'+night+' '+b+' '+str(int(np.mean(bdata)))+' '+str(int(np.median(bdata)))+' '+str(int(np.max(bdata)))+' '+str(int(np.min(bdata)))+' '+str(np.std(bdata))+' '+str(np.mean(data_per_master))+' '+str(np.median(data_per_master))+' '+str(np.std(data_per_master)))
+                bias_stack[i] = bdata
+                bias_stack_per_master[i] = data_per_master
+                i+=1
+    fout.write('\n# Combined ' + str(nbias) + 'bias for the date '+ night+' ...')
+    fout.write('\n# date mean median max min std mean_per_master median_per_master std_per_master')
+    print '# Combined ' + str(nbias) + ' bias for the date '+ night+' ...'
+    print '# date mean median max min std mean_per_master median_per_master std_per_master'
+    stacked_bias = np.median(bias_stack,axis=0)
+    stacked_bias_per_master = stacked_bias/mdata
+    fout.write('\n'+night+' '+str(int(np.mean(stacked_bias)))+' '+str(int(np.median(stacked_bias)))+' '+ str(int(np.max(stacked_bias)))+' '+ str(int(np.min(stacked_bias)))+' '+ str(np.std(stacked_bias))+' '+str(np.mean(stacked_bias_per_master))+' '+str(np.median(stacked_bias_per_master))+' '+str(np.std(stacked_bias_per_master)))
+    print night, int(np.mean(stacked_bias)), int(np.median(stacked_bias)), int(np.max(stacked_bias)), int(np.min(stacked_bias)), np.std(stacked_bias), np.mean(stacked_bias_per_master), np.median(stacked_bias_per_master), np.std(stacked_bias_per_master)
+    if((np.mean(stacked_bias_per_master) >= (1.0+acceptablePerc)) or (np.mean(stacked_bias_per_master) <= (1.0-acceptablePerc))):
+        print('WARNING: Bias for the date '+ night+'above acceptable value compared to master!')
+        fout.write('\n WARNING: Bias for the date '+ night+'above acceptable value compared to master! \n')
+    bias_day_mean.append(np.mean(stacked_bias_per_master))
+    bias_day_error.append(np.std(stacked_bias_per_master))
+    
+
 outputPlot(fulldatelist, actualdates, bias_day_mean, bias_day_error, settings['output']+'.png', acceptablePerc)
 fout.close()
